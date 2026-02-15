967f4d34cd206b9e3df1d73365f2c275c067470a
SAPNA CHOUDHARY <147359304+swapnachoudhary43@users.noreply.github.com>
2026-02-15 13:41:17 +0530

    Time: 3 ms (65.16%) | Memory: 7.6 MB (98.27%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+       if(n==0) return 0;
+       if (n==1) return 1;
+       int a=0, b=1,c;
+       for(int i=0;i<n;i++){
+         c= a+b;
+         b=a;
+         a=c;
+       }
+       return a;
+          
+     }
+};
