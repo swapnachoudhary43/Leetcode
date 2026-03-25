35aae6748a6982b54bd0636ad5af15b9a2f4fc66
SAPNA CHOUDHARY <147359304+swapnachoudhary43@users.noreply.github.com>
2026-03-25 21:42:54 +0530

    Time: 49 ms (52.13%) | Memory: 29 MB (91.43%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Skip duplicate nums[i]
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int l = i + 1;
+            int r = nums.size() - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum == 0) {
+                    res.push_back({nums[i], nums[l], nums[r]});
+
+                    // Skip duplicate nums[l]
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    // Skip duplicate nums[r]
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+
+                    l++;
+                    r--;
+                }
+                else if (sum < 0) {
+                    l++;
+                }
+                else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+};
