befa0697f91ad3f7ae4f3c4eac02591c0aa71e20
SWAPNA CHOUDHARY <147359304+swapnachoudhary43@users.noreply.github.com>
2026-06-27 18:24:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (17.94%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long low =0 ;
+        long long high = num;
+
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+
+            long long square = mid * mid;
+
+            if(square == num){
+                return true;
+            }
+            else if(square < num){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
